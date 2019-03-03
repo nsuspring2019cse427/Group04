022e13ae88f3a776fa8e94ab2dbe31a6f85b56c5
alif <alif@admin.com>
2019-03-03 11:59:20 +0600

    models work

--- Project01/Code/Base Code/teachme/models.py
+++ Project01/Code/Base Code/teachme/models.py
@@ -73,6 +73,26 @@ class StudentPost(models.Model):
 	
 	
 	
-	'''Spost  SpostedBy  SpostTime   SofferdMoney'''
+
+class Project(models.Model):
+	name = models.CharField(max_length=100)
+	slug = models.SlugField(max_length=100, unique=True, blank=True)
+	budget = models.IntegerField()
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Project, self).save(*args, **kwargs)
+
+	@property
+	def budget_left(self):
+		expense_list = Expense.objects.filter(project=self)
+		total_expense_amount = 0
+		for expense in expense_list:
+			total_expense_amount += expense.amount
+
+		# temporary solution, because the form currently only allows integer amounts
+		total_expense_amount = int(total_expense_amount)
+
+		return self.budget - total_expense_amount
 	
 	
