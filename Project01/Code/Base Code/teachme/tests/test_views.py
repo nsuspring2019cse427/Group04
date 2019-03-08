48804ded34d8576d3cb6454e7316fc42ca8bed4f
alif <alif@admin.com>
2019-03-08 11:40:50 +0600

    views_testing

---
+++ Project01/Code/Base Code/teachme/tests/test_views.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python
+
+from django.test import TestCase , Client
+from django.urls import reverse
+from teachme.models import Project , Category ,  Expense
+import json
+
+
+
+
+
+class TestViews(TestCase):
+	
+	
+	def setUp(self):
+		self.client = Client()
+		self.list_url = reverse('list')
+		self.detail_url = reverse('detail' , args=['project1'])
+		self.project1 = Project.objects.create(name = 'project1' , budget = 10000)
+	
+	
+	
+	def test_project_list_GET(self):
+		
+		
+		response = self.client.get(self.list_url)
+		
+		self.assertEquals(response.status_code,200)
+		self.assertTemplateUsed(response, 'budget_study.html')
+	
+	def testProject_details_get(self):
+		
+		
+		response = self.client.get(self.detail_url)
+		
+		self.assertEquals(response.status_code,200)
+		self.assertTemplateUsed(response, 'project-detail.html')
+		
+		
+		
+		
+		
+		
+		
+	def test_project_detail_POST_adds_new_expense(self):
+		
+		Category.objects.create(
+			project=self.project1 , 
+			name = 'development'
+		)
+		
+		
+		response = self.client.post(self.detail_url , {
+			
+			'title' : 'expense1' , 
+			'amount' : 10000 , 
+			'category' : 'development'
+		})
+		
+		
+		self.assertEquals(response.status_code , 302)
+		self.assertEquals(self.project1.expenses.first().title , 'expense1')
+		
+				
+		
+		
+		
+		
+	def test_project_detail_POST_no_deta(self):
+		response = self.client.post(self.detail_url)
+		self.assertEquals(response.status_code,302)
+		print("till done 2")
+		self.assertEquals(self.project1.expenses.count(), 0)
+		print("till done 3")
+		
+		
+		
+		
+	def test_project_detail_DELETE_deletes_expense(self):
+		
+		
+		category1 = Category.objects.create(
+			project=self.project1 , 
+			name = 'development'
+		)
+		
+		
+		
+		Expense.objects.create(
+			project = self.project1,
+			title='expense1',
+			amount=1000,
+			category = category1
+		)
+		
+		
+		
+		response = self.client.delete(self.detail_url, json.dumps({'id':1}))
+		
+		self.assertEqual(response.status_code, 204)
+		self.assertEqual(self.project1.expenses.count(),0)
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+		
+		
